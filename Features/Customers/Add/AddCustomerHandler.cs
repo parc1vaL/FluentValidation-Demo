e0edd8fe6eb4c349899a824d84b5febf52114b99
Patrick Schlarmann <pschlarm@posteo.de>
2021-11-29 19:52:19 +0100

    handler logging

--- Features/Customers/Add/AddCustomerHandler.cs
+++ Features/Customers/Add/AddCustomerHandler.cs
@@ -4,10 +4,18 @@ namespace FluentValidationDemo.Features.Customers.Add;
 
 public class AddCustomerHandler : AsyncRequestHandler<AddCustomerCommand>
 {
+    private readonly ILogger<AddCustomerHandler> logger;
+
+    public AddCustomerHandler(ILogger<AddCustomerHandler> logger)
+    {
+        this.logger = logger;
+    }
+
     protected override async Task Handle(
         AddCustomerCommand request, 
         CancellationToken cancellationToken)
     {
+        this.logger.LogInformation("Handling request...");
         // Handle request...
     }
 }
